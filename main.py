4af7106e61c11d21acb3dbceaee5f6fdbe476364
CorNit0 <ksyshayt@gmail.com>
2024-12-19 18:11:19 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+course = "Python"
+time_for_one_task = (hours / homeworks)
+print("Курс: ", course, ", всего задач:", homeworks, ", затрачено часов: ", hours, ", среднее время выполнения: ", time_for_one_task)
